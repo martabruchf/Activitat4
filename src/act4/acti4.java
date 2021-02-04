430636e9df9675535a1f849219667466cf294406
Marta Bruch <marta.bruch@hotmail.com>
2021-02-04 16:50:35 +0100

    Segon commit

---
+++ src/act4/acti4.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package act4;
+
+/**
+ * @author marta
+ *
+ */
+public class acti4 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
